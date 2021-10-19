4b72da1440281c4c658b8f425c2d827086edbb16
Jana Rezabkova <jana.rezabkova@gmail.com>
2021-10-19 22:43:39 +0200

    Use empty init file

--- brain_connectivity/__init__.py
+++
@@ -1,5 +0,0 @@
-# __all__ = ['Model', 'ModelType', 'ConnectivityDenseNet', 'ConnectivityMode', 'GIN']
-
-# from brain_connectivity.model import Model, ModelType
-# from brain_connectivity.connectivity_dense_net import ConnectivityDenseNet, ConnectivityMode
-# from brain_connectivity.gin import GIN
