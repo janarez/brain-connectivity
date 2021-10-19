d9868bf25181f2f38e661402c3137a9850c9adcd
Jana Rezabkova <jana.rezabkova@gmail.com>
2021-10-19 21:09:46 +0200

    Add comment regarding API

--- brain_connectivity/data_utils.py
+++ brain_connectivity/data_utils.py
@@ -11,5 +11,6 @@ def zeroth_axis_sample(matrix, i):
     return torch.from_numpy(matrix[i]).to(torch.float32)
 
 
-def identity_matrix(size, i):
+def identity_matrix(size, _):
+    "Keep last parameter for index in unified API."
     return torch.diag(torch.ones(size))
