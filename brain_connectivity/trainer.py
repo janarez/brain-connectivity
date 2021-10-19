003699f39027af5bb8fa86c3af88c7cdc044bcbe
Jana Rezabkova <jana.rezabkova@gmail.com>
2021-10-19 21:12:38 +0200

    Log training params on init

--- brain_connectivity/trainer.py
+++ brain_connectivity/trainer.py
@@ -1,9 +1,9 @@
-from typing import Union, List
+import os
 
 import torch
-import torch.nn as nn
 from torch.utils.tensorboard.writer import SummaryWriter
 from torch_geometric.data.dataloader import DataLoader
+from torchinfo.torchinfo import summary
 
 from .evaluation import ModelEvaluation
 
@@ -11,11 +11,11 @@ from .evaluation import ModelEvaluation
 class Trainer():
     def __init__(
         self,
+        log_folder,
         # Already initiated model.
         model,
         trainloader: DataLoader,
         valloader: DataLoader,
-        writer: SummaryWriter,
         epochs: int,
         validation_frequency: int,
         fc_matrix_plot_frequency: int,
@@ -34,8 +34,9 @@ class Trainer():
         self.scheduler = scheduler(self.optimizer, **scheduler_kwargs)
         self.criterion = criterion
 
-        self.writer = writer
-        self.evaluation = ModelEvaluation(writer)
+        self.writer = SummaryWriter(log_folder)
+        self.evaluation = ModelEvaluation(self.writer)
+        self.log_folder = log_folder
 
         self.epochs = epochs
         self.validation_frequency = validation_frequency
@@ -43,6 +44,17 @@ class Trainer():
         self.fc_matrix_plot_frequency = fc_matrix_plot_frequency
         self.fc_matrix_plot_sublayer = fc_matrix_plot_sublayer
 
+        # Save to file before training.
+        self._log()
+
+    def _log(self):
+        """
+        Logs all important training parameters and model to a file.
+        """
+        with open(os.path.join(self.log_folder, 'trainer.txt'), 'w', encoding="utf-8") as f:
+            f.write(self.__dict__.__str__() + '\n')
+            f.write(self.model.__str__() + '\n\n')
+            f.write(str(summary(self.model)))
 
     def train(self):
         for epoch in range(self.epochs):
@@ -57,7 +69,8 @@ class Trainer():
 
             # Plot connectivity matrix.
             if (epoch+1) % self.fc_matrix_plot_frequency == 0:
-                self.model.plot_fc_matrix(epoch, sublayer=self.fc_matrix_plot_sublayer)
+                self.model.plot_fc_matrix(
+                    epoch, sublayer=self.fc_matrix_plot_sublayer)
 
             # Update learning rate.
             self.scheduler.step()
@@ -65,31 +78,29 @@ class Trainer():
             # Return results for best accuracy.
             return self.evaluation.get_best_results()
 
-
     def _epoch_step(self, dataloader: DataLoader, epoch: int, evaluate: bool = False):
-            running_loss = 0.
+        running_loss = 0.
+        if evaluate:
+            self.model.eval()
+        else:
+            self.model.train()
+
+        for data in dataloader:
+            loss, outputs = self._model_step(data, backpropagate=not evaluate)
+            running_loss += loss
+
+            # Calculate evaluation metrics.
             if evaluate:
-                self.model.eval()
-            else:
-                self.model.train()
+                predicted = outputs.argmax(dim=1)
+                # labels = torch.nonzero(data.y, as_tuple=True)[1]
+                labels = data.y.view(-1)
+                self.evaluation.evaluate(predicted, labels)
 
-            for data in dataloader:
-                loss, outputs = self._model_step(data, backpropagate=not evaluate)
-                running_loss += loss
-
-                # Calculate evaluation metrics.
-                if evaluate:
-                    predicted = outputs.argmax(dim=1)
-                    # labels = torch.nonzero(data.y, as_tuple=True)[1]
-                    labels = data.y.view(-1)
-                    self.evaluation.evaluate(predicted, labels)
-
-            epoch_loss = running_loss / len(dataloader)
-            self.writer.add_scalar(
-                f'{"Validation" if evaluate else "Training"} loss',
-                epoch_loss, epoch
-            )
-
+        epoch_loss = running_loss / len(dataloader)
+        self.writer.add_scalar(
+            f'{"Validation" if evaluate else "Training"} loss',
+            epoch_loss, epoch
+        )
 
     def _model_step(self, data, backpropagate: bool = True):
         self.optimizer.zero_grad()
