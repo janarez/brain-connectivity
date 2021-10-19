a47227f0d6b1749d6faa5a70d1fe0020e06ebbde
Jana Rezabkova <jana.rezabkova@gmail.com>
2021-10-19 22:53:55 +0200

    Add dataloaders

--- brain_connectivity/dataset.py
+++ brain_connectivity/dataset.py
@@ -10,53 +10,54 @@ import operator
 import torch
 from torch.utils.data import TensorDataset
 from torch_geometric.data import Data, DataLoader
+from sklearn.model_selection import StratifiedKFold
 
 from .enums import CorrelationType, NodeFeatures, ThresholdingFunction, DataThresholdingType
-from .fc_matrix import get_data_threshold_at_largest_average_difference_between_groups, \
-    get_data_thresholded_by_explicit_matrix, get_data_thresholded_by_random_matrix, \
-    get_data_thresholded_by_sample_values
+from .fc_matrix import (
+    get_data_threshold_at_largest_average_difference_between_groups,
+    get_data_thresholded_by_explicit_matrix,
+    get_data_thresholded_by_random_matrix,
+    get_data_thresholded_by_sample_values,
+)
 from .data_utils import dotdict, identity_matrix, zeroth_axis_sample
 
 
 class FunctionalConnectivityDataset:
-
     def __init__(
         self,
         log_folder,
-        folder: os.Path,
-        dataframe_with_subjects: str = 'patients_cleaned.csv',
-        target_column: str = 'target',
+        folder,
+        dataframe_with_subjects: str = "patients-cleaned.csv",
+        target_column: str = "target",
         correlation_type: CorrelationType = CorrelationType.PEARSON,
         node_features: NodeFeatures = NodeFeatures.FC_MATRIX_ROW,
         test_size: Union[int, float] = 50,
         batch_size: int = 8,
-        random_seed: int = 42
+        num_folds: int = 7,
+        random_seed: int = 42,
     ):
         self.log_folder = log_folder
         self.base_folder = folder
-        self.df = pd.read_csv(os.path.join(
-            self.base_folder, dataframe_with_subjects), index_col=0)
 
+        df = pd.read_csv(os.path.join(self.base_folder, dataframe_with_subjects), index_col=0)
         self.raw_fc_matrices = self._read_raw_matrices(correlation_type)
-        self.targets = self.df[target_column].values
+        self.targets = df[target_column].values
 
         self.num_subjects, self.num_regions, _ = self.raw_fc_matrices.shape
         self.batch_size = batch_size
+        self.skf = StratifiedKFold(n_splits=num_folds, random_state=random_seed, shuffle=True)
 
         self.random_seed = random_seed
-        self.rgn = np.random.default_rng(random_seed)
+        self.rng = np.random.default_rng(random_seed)
 
         # Select how node features are obtained.
         if node_features == NodeFeatures.FC_MATRIX_ROW:
-            self.get_node_features = partial(
-                zeroth_axis_sample, self.raw_fc_matrices)
+            self.get_node_features = partial(zeroth_axis_sample, self.raw_fc_matrices)
         elif node_features == NodeFeatures.ONE_HOT_REGION:
-            self.get_node_features = partial(
-                identity_matrix, self.num_regions)
+            self.get_node_features = partial(identity_matrix, self.num_regions)
 
         # Split data into hidden test set and training (dev+train) set.
-        self.train_indices, self.test_indices = self._train_test_split(
-            test_size=test_size)
+        self.dev_indices, self.test_indices = self._dev_test_split(test_size=test_size)
 
         # Save to file before training.
         self._log()
@@ -65,15 +66,15 @@ class FunctionalConnectivityDataset:
         """
         Logs all important information about used dataset to a file.
         """
-        with open(os.path.join(self.log_folder, 'dataset.txt'), 'w', encoding="utf-8") as f:
+        with open(os.path.join(self.log_folder, "dataset.txt"), "w", encoding="utf-8") as f:
             f.write(self.__dict__.__str__())
 
     def _read_raw_matrices(self, correlation_type: CorrelationType):
-        path = os.path.join(self.base_folder, f'fc-{correlation_type}.pickle')
-        with open(path, 'rb') as f_rb:
+        path = os.path.join(self.base_folder, f"fc-{correlation_type}.pickle")
+        with open(path, "rb") as f_rb:
             f = pickle.load(f_rb)
 
-        logging.debug(f'Loaded dataset with shape {f.shape}')
+        logging.debug(f"Loaded dataset with shape {f.shape}")
         return f
 
     def create_connectivity_dataset(
@@ -83,7 +84,7 @@ class FunctionalConnectivityDataset:
         threshold: Union[float, int],
         thresholding_operator: Optional[Union[operator.le, operator.ge]] = operator.ge,
         threshold_by_absolute_value: bool = True,
-        return_absolute_value: bool = False
+        return_absolute_value: bool = False,
     ):
         if thresholding_function == ThresholdingFunction.GROUP_AVERAGE:
             b, r = get_data_threshold_at_largest_average_difference_between_groups(
@@ -92,7 +93,7 @@ class FunctionalConnectivityDataset:
                 train_indices=None,
                 threshold_type=threshold_type,
                 threshold=threshold,
-                thresholding_operator=thresholding_operator
+                thresholding_operator=thresholding_operator,
             )
         elif thresholding_function == ThresholdingFunction.SUBJECT_VALUES:
             b, r = get_data_thresholded_by_sample_values(
@@ -101,7 +102,7 @@ class FunctionalConnectivityDataset:
                 threshold=threshold,
                 thresholding_operator=thresholding_operator,
                 threshold_by_absolute_value=threshold_by_absolute_value,
-                return_absolute_value=return_absolute_value
+                return_absolute_value=return_absolute_value,
             )
         elif thresholding_function == ThresholdingFunction.EXPLICIT_MATRIX:
             b, r = get_data_thresholded_by_explicit_matrix(
@@ -111,7 +112,7 @@ class FunctionalConnectivityDataset:
                 threshold=threshold,
                 thresholding_operator=thresholding_operator,
                 threshold_by_absolute_value=threshold_by_absolute_value,
-                return_absolute_value=return_absolute_value
+                return_absolute_value=return_absolute_value,
             )
         elif thresholding_function == ThresholdingFunction.RANDOM:
             b, r = get_data_thresholded_by_random_matrix(
@@ -121,47 +122,40 @@ class FunctionalConnectivityDataset:
                 threshold=threshold,
                 thresholding_operator=thresholding_operator,
                 threshold_by_absolute_value=threshold_by_absolute_value,
-                return_absolute_value=return_absolute_value
+                return_absolute_value=return_absolute_value,
             )
 
         self.binary_fc_matrices = b
         self.real_fc_matrices = r
 
-    def _train_test_split(self, test_size):
-        patient_indexes = self.df[self.df[self.target_column] == 1].index
-        control_indexes = self.df[self.df[self.target_column] == 0].index
+    def _dev_test_split(self, test_size):
+        patient_indexes = np.where(self.targets == 1)
+        control_indexes = np.where(self.targets == 0)
 
         if type(test_size) == float:
             test_size = round(self.num_subjects * test_size)
 
-        test_patients = self.rng.choice(
-            patient_indexes, size=test_size//2, replace=False)
-        test_controls = self.rng.choice(
-            control_indexes, size=test_size//2, replace=False)
+        test_patients = self.rng.choice(patient_indexes, size=test_size // 2, replace=False)
+        test_controls = self.rng.choice(control_indexes, size=test_size // 2, replace=False)
 
         test_indices = np.hstack([test_patients, test_controls])
-        train_indices = list(
-            set(range(self.num_subjects)) - set(test_indices))
+        train_indices = list(set(range(self.num_subjects)) - set(test_indices))
 
-        assert len(test_indices) + len(
-            train_indices) == self.num_subjects, 'Dataset splitting created / lost samples.'
-        logging.debug(f'Test size: {len(test_indices)}')
-        logging.debug(
-            f'Test group 1 percantage: {sum(test_indices) / len(test_indices) * 100:.2f}')
-        logging.debug(f'Train size: {len(train_indices)}')
-        logging.debug(
-            f'Train group 1 percantage: {sum(train_indices) / len(train_indices) * 100:.2f}')
+        assert len(test_indices) + len(train_indices) == self.num_subjects, "Dataset splitting created / lost samples."
+        logging.debug(f"Test size: {len(test_indices)}")
+        logging.debug(f"Test group 1 percantage: {sum(test_indices) / len(test_indices) * 100:.2f}")
+        logging.debug(f"Train size: {len(train_indices)}")
+        logging.debug(f"Train group 1 percantage: {sum(train_indices) / len(train_indices) * 100:.2f}")
 
         return train_indices, test_indices
 
-    def _get_dense_dataset(self, train_set: bool):
-        indices = self.train_indices if train_set else self.test_indices
+    def _get_dense_dataset(self, indices):
         return TensorDataset(
             torch.from_numpy(self.raw_fc_matrices[indices]).to(torch.float32),
-            torch.from_numpy(self.targets[indices]).to(torch.int64)
+            torch.from_numpy(self.targets[indices]).to(torch.int64),
         )
 
-    def _get_geometric_dataset(self, train_set: bool):
+    def _get_geometric_dataset(self, indices):
         """
         `Data` object fields
 
@@ -171,35 +165,53 @@ class FunctionalConnectivityDataset:
         - `data.y`: Target to train against (may have arbitrary shape), e.g., node-level targets of shape `[num_nodes, *]` or graph-level targets of shape `[1, *]`
         - `data.pos`: Node position matrix with shape `[num_nodes, num_dimensions]`
         """
-        indices = self.train_indices if train_set else self.test_indices
-        return [Data(
-            x=self.get_node_features(i),
-            edge_index=torch.from_numpy(np.asarray(
-                np.nonzero(self.binary_fc_matrices[i]))).to(torch.int64),
-            y=torch.tensor([target], dtype=torch.int64)
-        ) for target, i in zip(self.targets[indices], indices)
+        return [
+            Data(
+                x=self.get_node_features(i),
+                edge_index=torch.from_numpy(np.asarray(np.nonzero(self.binary_fc_matrices[i]))).to(torch.int64),
+                y=torch.tensor([target], dtype=torch.int64),
+            )
+            for target, i in zip(self.targets[indices], indices)
         ]
 
     @property
     def geometric_trainloader(self):
-        pass
+        return DataLoader(self._get_geometric_dataset(self.train_indices), batch_size=self.batch_size, shuffle=True)
 
     @property
     def geometric_valloader(self):
-        pass
+        return DataLoader(self._get_geometric_dataset(self.val_indices), batch_size=self.batch_size, shuffle=False)
 
     @property
     def geometric_testloader(self):
-        return DataLoader(self._get_geometric_dataset(train_set=False), batch_size=self.batch_size, shuffle=False)
+        return DataLoader(self._get_geometric_dataset(self.test_indices), batch_size=self.batch_size, shuffle=False)
 
     @property
     def dense_trainloader(self):
-        pass
+        return list(
+            map(
+                dotdict,
+                DataLoader(self._get_dense_dataset(self.train_indices), batch_size=self.batch_size, shuffle=True),
+            )
+        )
 
     @property
     def dense_valloader(self):
-        pass
+        return list(
+            map(
+                dotdict,
+                DataLoader(self._get_dense_dataset(self.val_indices), batch_size=self.batch_size, shuffle=False),
+            )
+        )
 
     @property
     def dense_testloader(self):
-        return list(map(dotdict, DataLoader(self._get_dense_dataset(train_set=False), batch_size=self.batch_size, shuffle=False)))
+        return list(
+            map(
+                dotdict,
+                DataLoader(self._get_dense_dataset(self.test_indices), batch_size=self.batch_size, shuffle=False),
+            )
+        )
+
+    def next_fold(self):
+        _, (self.train_indices, self.val_indices) = next(self.skf(self.dev_indices, self.targets[self.train_indices]))
