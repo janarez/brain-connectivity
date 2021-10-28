52e9480a3c206acd239345d44954ac3fdd179f9f
Jana Rezabkova <jana.rezabkova@gmail.com>
2021-10-28 14:15:42 +0200

    Document enums

--- brain_connectivity/enums.py
+++ brain_connectivity/enums.py
@@ -1,8 +1,11 @@
-from enum import Enum, auto
+"""
+Collection of all enums.
 
+We extract all defined enums here, to enable easy debugging in Jupyter
+using the `reload` module function.
 """
-We extract all defined enums here, to enable easy debugging in Jupyter using the `reload` module function.
-"""
+
+from enum import Enum, auto
 
 
 class ConnectivityMode(Enum):
@@ -22,21 +25,39 @@ class ConnectivityMode(Enum):
 
 
 class CorrelationType(Enum):
-    PEARSON = 'pearson'
-    SPEARMAN = 'spearman'
-    PARTIAL_PEARSON = 'partial_pearson'
+    "Different types of correlation for the raw timeseries."
+    PEARSON = "pearson"
+    SPEARMAN = "spearman"
+    PARTIAL_PEARSON = "partial_pearson"
 
     def __str__(self):
-        return f'{self.value}'
+        return f"{self.value}"
 
 
 class DataThresholdingType(Enum):
+    """
+    Determines method of creating graph from a FC matrix.
+
+    KNN: Take top `k` edges of each node.
+    FIXED_THRESHOLD: Take all edges crossing a threshold.
+    SPARSITY: Take top `x` % of edges from full matrix.
+    """
+
     KNN = auto()
     FIXED_THRESHOLD = auto()
     SPARSITY = auto()
 
 
 class ThresholdingFunction(Enum):
+    """
+    Determines based on what information are graphs created.
+
+    GROUP_AVERAGE: Threshold using matrix created as average difference between the two groups.
+    SUBJECT_VALUES: Threshold on matrix of each subject.
+    EXPLICIT_MATRIX: Threshold on some given matrix instead of data.
+    RANDOM: Threshold on random FC matrix instead of data.
+    """
+
     GROUP_AVERAGE = auto()
     SUBJECT_VALUES = auto()
     EXPLICIT_MATRIX = auto()
@@ -44,5 +65,10 @@ class ThresholdingFunction(Enum):
 
 
 class NodeFeatures(Enum):
+    """
+    FC_MATRIX_ROW: Each node has as its features its row from FC matrix.
+    ONE_HOT_REGION: Each node has as its features one hot encoding of its id.
+    """
+
     FC_MATRIX_ROW = auto()
     ONE_HOT_REGION = auto()
