7c77c6dcd89691278a8530b215d2fa0a0cd32598
Jana Rezabkova <jana.rezabkova@gmail.com>
2021-11-07 17:44:28 +0100

    Parametrize embedding initialization

--- brain_connectivity/dense.py
+++ brain_connectivity/dense.py
@@ -18,14 +18,23 @@ class ConnectivityEmbedding(nn.Module):
     Output: [batch_size, num_nodes, num_features]
     """
 
-    def __init__(self, size, dropout, residual):
+    def __init__(self, size, dropout, residual, init_weights, val, std):
         super(ConnectivityEmbedding, self).__init__()
         # Initialize with fully connected graph.
         self.fc_matrix = nn.Parameter(torch.empty(size, size), requires_grad=True)
-        # nn.init.uniform_(self.fc_matrix, a=-1/size, b=1/size)
-        # nn.init.normal_(self.fc_matrix, mean=0, std=0.1)
-        nn.init.constant_(self.fc_matrix, val=1.0)
-        # nn.init.sparse_(self.fc_matrix, sparsity=0.5)
+
+        if init_weights == "constant":
+            nn.init.constant_(self.fc_matrix, val=val)
+        elif init_weights == "normal":
+            nn.init.normal_(self.fc_matrix, mean=val, std=std)
+        elif init_weights == "uniform":
+            nn.init.uniform_(self.fc_matrix, a=-val, b=val)
+        elif init_weights == "sparse":
+            nn.init.sparse_(self.fc_matrix, sparsity=val, std=std)
+        else:
+            raise ValueError(
+                f"Found {init_weights}, expected one of: 'constant', 'normal', 'uniform' or 'sparse' for param `init_weights`"
+            )
 
         self.dropout = nn.Dropout(p=dropout)
         self.residual = residual
@@ -81,19 +90,18 @@ class ConnectivitySublayer(nn.Module):
         size_emb: int,
         dropout: float,
         mode: ConnectivityMode,
-        **mode_kwargs
+        mode_kwargs: dict,
+        emb_matrix: Optional[ConnectivityEmbedding] = None,
     ):
         super(ConnectivitySublayer, self).__init__()
 
         self.id = sublayer_id
         # Create new FC matrix for this sublayer.
         if mode == ConnectivityMode.MULTIPLE:
-            self.fc_matrix = ConnectivityEmbedding(
-                size_emb, dropout=mode_kwargs["fc_dropout"], residual=mode_kwargs["fc_residual"]
-            )
+            self.fc_matrix = ConnectivityEmbedding(size_emb, **mode_kwargs)
         # Used passed in FC matrix.
         else:
-            self.fc_matrix = mode_kwargs["fc_matrix"]
+            self.fc_matrix = emb_matrix
             # Switch of gradients for subsequent layers in start mode.
             if mode == mode.START and sublayer_id > 0:
                 self.fc_matrix.toggle_gradients(requires_grad=False)
@@ -124,16 +132,26 @@ class ConnectivityDenseNet(Model):
         size_in: int,
         num_hidden_features: Union[int, List[int]],
         dropout: float = 0.5,
-        connectivity_dropout: float = 0.0,
-        connectivity_residual: Optional[str] = None,
+        emb_dropout: float = 0.0,
+        emb_residual: Optional[str] = "mean",
+        emb_init_weights: str = "constant",
+        emb_val: float = 0.0,
+        emb_std: float = 0.01,
         num_sublayers: int = 3,
         readout: str = "add",
-        **mode_kwargs
+        **mode_kwargs,
     ):
         super(ConnectivityDenseNet, self).__init__()
 
         self.mode = mode
         self.fc_matrix = None
+        emb_kwargs = {
+            "dropout": emb_dropout,
+            "residual": emb_residual,
+            "init_weights": emb_init_weights,
+            "val": emb_val,
+            "std": emb_std,
+        }
         # Set passed in FC matrix.
         if mode == ConnectivityMode.FIXED:
             self.fc_matrix = lambda x: torch.matmul(mode_kwargs["fc_matrix"], x)
@@ -141,15 +159,8 @@ class ConnectivityDenseNet(Model):
         # or
         # Create single FC matrix that will be learned throughout.
         elif (mode == ConnectivityMode.START) or (mode == ConnectivityMode.SINGLE):
-            self.fc_matrix = ConnectivityEmbedding(
-                num_nodes, dropout=connectivity_dropout, residual=connectivity_residual
-            )
+            self.fc_matrix = ConnectivityEmbedding(num_nodes, **emb_kwargs)
         # Else `ConnectivityMode.MULTIPLE`, let each sublayer create its own FC matrix.
-        self.mode_kwargs = {
-            "fc_matrix": self.fc_matrix,
-            "fc_dropout": connectivity_dropout,
-            "fc_residual": connectivity_residual,
-        }
 
         # Prepare feature mapping dimensions.
         if type(num_hidden_features) is int:
@@ -168,7 +179,8 @@ class ConnectivityDenseNet(Model):
                     size_emb=num_nodes,
                     dropout=dropout,
                     mode=mode,
-                    **self.mode_kwargs
+                    mode_kwargs=emb_kwargs,
+                    emb_matrix=self.fc_matrix,
                 )
                 for i, (size_in, size_out) in enumerate(zip(num_in_features, num_out_features))
             ]
