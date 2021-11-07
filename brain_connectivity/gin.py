ce6305f227acca23d0a0ca40f6c73202e3193cdd
Jana Rezabkova <jana.rezabkova@gmail.com>
2021-11-07 17:45:08 +0100

    Fix array concat bug

--- brain_connectivity/gin.py
+++ brain_connectivity/gin.py
@@ -1,10 +1,11 @@
 from typing import List, Union
-import numpy as np
 
+import numpy as np
 import torch
 import torch.nn as nn
-
 from torch_geometric.nn import GINConv, global_add_pool
+
+from .model import Model
 
 
 class GinMLP(nn.Module):
@@ -19,15 +20,14 @@ class GinMLP(nn.Module):
         return self.activation(self.dropout(self.fc(x)))
 
 
-class GIN(torch.nn.Module):
-
+class GIN(Model):
     def __init__(
         self,
         size_in: int,
         num_hidden_features: Union[List[int], int] = 90,
         dropout: float = 0.5,
         num_sublayers: int = 3,
-        eps: float=0.0
+        eps: float = 0.0,
     ):
         super(GIN, self).__init__()
 
@@ -36,11 +36,14 @@ class GIN(torch.nn.Module):
             num_out_features = np.repeat(num_hidden_features, num_sublayers)
         else:
             num_out_features = num_hidden_features
-        num_in_features = [size_in] + num_out_features
+        num_in_features = np.hstack([[size_in], num_out_features])
 
-        self.convs = torch.nn.ModuleList([
-            GINConv(nn=GinMLP(size_in, size_out, dropout=dropout), eps=eps) for size_in, size_out in zip(num_in_features, num_out_features)
-        ])
+        self.convs = torch.nn.ModuleList(
+            [
+                GINConv(nn=GinMLP(size_in, size_out, dropout=dropout), eps=eps)
+                for size_in, size_out in zip(num_in_features, num_out_features)
+            ]
+        )
         self.fc = nn.Linear(num_out_features[-1], 2)
 
     def forward(self, data):
