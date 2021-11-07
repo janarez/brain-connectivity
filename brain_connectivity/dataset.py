8f715c9f5ff5ddc426f2c010e699aee2773c278f
Jana Rezabkova <jana.rezabkova@gmail.com>
2021-11-07 12:07:46 +0100

    Fix fold generation

--- brain_connectivity/dataset.py
+++ brain_connectivity/dataset.py
@@ -16,10 +16,15 @@ from .fc_matrix import create_connectivity_matrices
 
 
 class FunctionalConnectivityDataset:
+    """
+    TODO: Write class docstring.
+    """
+
     def __init__(
         self,
         log_folder,
-        folder,
+        data_folder,
+        device,
         dataframe_with_subjects: str = "patients-cleaned.csv",
         target_column: str = "target",
         correlation_type: CorrelationType = CorrelationType.PEARSON,
@@ -31,15 +36,15 @@ class FunctionalConnectivityDataset:
         geometric_dataset_kwargs: dict = {},
     ):
         self.log_folder = log_folder
-        self.base_folder = folder
+        self.data_folder = data_folder
+        self.device = device
 
-        df = pd.read_csv(os.path.join(self.base_folder, dataframe_with_subjects), index_col=0)
+        df = pd.read_csv(os.path.join(self.data_folder, dataframe_with_subjects), index_col=0)
         self.raw_fc_matrices = self._read_raw_matrices(correlation_type)
         self.targets = df[target_column].values
 
         self.num_subjects, self.num_regions, _ = self.raw_fc_matrices.shape
         self.batch_size = batch_size
-        self.skf = StratifiedKFold(n_splits=num_folds, random_state=random_seed, shuffle=True)
 
         self.random_seed = random_seed
         self.rng = np.random.default_rng(random_seed)
@@ -49,8 +54,12 @@ class FunctionalConnectivityDataset:
         self.geometric_dataset_kwargs = geometric_dataset_kwargs
 
         # Split data into hidden test set and dev (train+validation) set.
-        self.dev_indices, self.test_indices = self._dev_test_split(test_size=test_size)
-        self.train_indices, self.val_indices = self.next_fold()
+        self.num_folds = num_folds
+        self.skf = StratifiedKFold(n_splits=num_folds, random_state=random_seed, shuffle=True)
+        self.dev_indices, self.test_indices, self.dev_fold_iterator = self._dev_test_split(
+            test_size=test_size
+        )
+        self.train_indices, self.val_indices = None, None
 
         # Save to file before training.
         self._log()
@@ -59,11 +68,16 @@ class FunctionalConnectivityDataset:
         """
         Logs all important information about used dataset to a file.
         """
+        # try:
+        #     os.makedirs(self.log_folder, exist_ok=False)
+        # except FileExistsError as e:
+        #     raise ValueError(f"Run experiment with same NAME and ID ({self.log_folder}).") from e
+
         with open(os.path.join(self.log_folder, "dataset.txt"), "w", encoding="utf-8") as f:
             f.write(self.__dict__.__str__())
 
     def _read_raw_matrices(self, correlation_type: CorrelationType):
-        path = os.path.join(self.base_folder, f"fc-{correlation_type}.pickle")
+        path = os.path.join(self.data_folder, f"fc-{correlation_type}.pickle")
         with open(path, "rb") as f_rb:
             f = pickle.load(f_rb)
 
@@ -81,15 +95,19 @@ class FunctionalConnectivityDataset:
         test_controls = self.rng.choice(control_indexes, size=test_size // 2, replace=False)
 
         test_indices = np.hstack([test_patients, test_controls])
-        train_indices = np.array(list(set(range(self.num_subjects)) - set(test_indices)))
+        dev_indices = np.array(list(set(range(self.num_subjects)) - set(test_indices)))
 
-        assert len(test_indices) + len(train_indices) == self.num_subjects, "Dataset splitting created / lost samples."
+        assert (
+            len(test_indices) + len(dev_indices) == self.num_subjects
+        ), "Dataset splitting created / lost samples."
         logging.debug(f"Test size: {len(test_indices)}")
         logging.debug(f"Test group 1 percantage: {sum(test_indices) / len(test_indices) * 100:.2f}")
-        logging.debug(f"Train size: {len(train_indices)}")
-        logging.debug(f"Train group 1 percantage: {sum(train_indices) / len(train_indices) * 100:.2f}")
+        logging.debug(f"Dev size: {len(dev_indices)}")
+        logging.debug(f"Dev group 1 percantage: {sum(dev_indices) / len(dev_indices) * 100:.2f}")
 
-        return train_indices, test_indices
+        # Save split indices, along with stratified training iterator.
+        dev_fold_iterator = self.skf.split(dev_indices, self.targets[dev_indices])
+        return dev_indices, test_indices, dev_fold_iterator
 
     def _get_node_features_function(self):
         # Each node contains its row of the raw correlation matrix.
@@ -99,14 +117,16 @@ class FunctionalConnectivityDataset:
         elif self.node_features == NodeFeatures.ONE_HOT_REGION:
             node_features_function = partial(identity_matrix, self.num_regions)
         else:
-            raise ValueError(f"Unknown value of `node_features` - ({self.node_features}). Use the `NodeFeatures` enum.")
+            raise ValueError(
+                f"Unknown value of `node_features` - ({self.node_features}). Use the `NodeFeatures` enum."
+            )
         return node_features_function
 
     def _get_dense_dataset(self, indices):
         node_features_function = self._get_node_features_function()
         return DenseDataset(
-            node_features_function(indices).to(torch.float32),
-            torch.from_numpy(self.targets[indices]).to(torch.int64),
+            node_features_function(indices).to(torch.float32).to(self.device),
+            torch.from_numpy(self.targets[indices]).to(torch.int64).to(self.device),
         )
 
     def _get_geometric_dataset(self, indices):
@@ -121,7 +141,7 @@ class FunctionalConnectivityDataset:
         """
         # We create FC data every time, since the average method needs to be calculated on training data only (i.e. before each fold)
         # and the overhead is small.
-        binary_fc_matrices, real_fc_matrices = create_connectivity_matrices(
+        binary_fc_matrices, _ = create_connectivity_matrices(
             self.raw_fc_matrices,
             **self.geometric_dataset_kwargs,
             binary_targets=self.targets,
@@ -132,53 +152,99 @@ class FunctionalConnectivityDataset:
         return [
             Data(
                 x=node_features_function([i]).squeeze(0),
-                edge_index=torch.from_numpy(np.asarray(np.nonzero(binary_fc_matrices[i]))).to(torch.int64),
-                y=torch.tensor([target], dtype=torch.int64),
-            )
+                edge_index=torch.from_numpy(np.asarray(np.nonzero(binary_fc_matrices[i]))).to(
+                    torch.int64
+                ),
+                y=torch.tensor([target], dtype=torch.int64),  # pylint: disable=not-callable
+            ).to(self.device)
             for target, i in zip(self.targets[indices], indices)
         ]
 
     @property
     def geometric_trainloader(self):
-        return DataLoader(self._get_geometric_dataset(self.train_indices), batch_size=self.batch_size, shuffle=True)
+        "Train dataloader with data for graph neural network."
+        return DataLoader(
+            self._get_geometric_dataset(self.train_indices),
+            batch_size=self.batch_size,
+            shuffle=True,
+        )
 
     @property
     def geometric_valloader(self):
-        return DataLoader(self._get_geometric_dataset(self.val_indices), batch_size=self.batch_size, shuffle=False)
+        "Validation dataloader with data for graph neural network."
+        return DataLoader(
+            self._get_geometric_dataset(self.val_indices), batch_size=self.batch_size, shuffle=False
+        )
 
     @property
     def geometric_testloader(self):
-        return DataLoader(self._get_geometric_dataset(self.test_indices), batch_size=self.batch_size, shuffle=False)
+        "Test dataloader with data for graph neural network."
+        return DataLoader(
+            self._get_geometric_dataset(self.test_indices),
+            batch_size=self.batch_size,
+            shuffle=False,
+        )
 
     @property
     def dense_trainloader(self):
+        "Train dataloader with data for dense neural network."
         return list(
             map(
                 dotdict,
-                DataLoader(self._get_dense_dataset(self.train_indices), batch_size=self.batch_size, shuffle=True),
+                DataLoader(
+                    self._get_dense_dataset(self.train_indices),
+                    batch_size=self.batch_size,
+                    shuffle=True,
+                ),
             )
         )
 
     @property
     def dense_valloader(self):
-        # We need to map to `dotdict` here, because `DataLoader` always returns plain dict from batch collating.
+        "Validation dataloader with data for dense neural network."
+        # We need to map to `dotdict` here,
+        # because `DataLoader` always returns plain dict from batch collating.
         return list(
             map(
                 dotdict,
-                DataLoader(self._get_dense_dataset(self.val_indices), batch_size=self.batch_size, shuffle=False),
+                DataLoader(
+                    self._get_dense_dataset(self.val_indices),
+                    batch_size=self.batch_size,
+                    shuffle=False,
+                ),
             )
         )
 
     @property
     def dense_testloader(self):
+        "Test dataloader with data for dense neural network."
         return list(
             map(
                 dotdict,
-                DataLoader(self._get_dense_dataset(self.test_indices), batch_size=self.batch_size, shuffle=False),
+                DataLoader(
+                    self._get_dense_dataset(self.test_indices),
+                    batch_size=self.batch_size,
+                    shuffle=False,
+                ),
             )
         )
 
     def next_fold(self):
-        "Updates train and validation indices to that of next stratified fold."
-        train_split, val_split = next(self.skf.split(self.dev_indices, self.targets[self.dev_indices]))
-        return self.dev_indices[train_split], self.dev_indices[val_split]
+        """
+        Iterator function that updates train and validation indices to that of next stratified fold.
+        If the fold generator is exhausted returns `False` else `True`.
+
+        To be used in a `while` cycle.
+        """
+        try:
+            train_split, val_split = next(self.dev_fold_iterator)
+            self.train_indices, self.val_indices = (
+                self.dev_indices[train_split],
+                self.dev_indices[val_split],
+            )
+        except StopIteration:
+            return False
+
+        # TODO: Log correct fold ID.
+        logging.info(f"Generated fold {1}/{self.num_folds}")
+        return True
