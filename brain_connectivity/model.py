c02aec7afea527ef7c78445c883b0eb4857af358
Jana Rezabkova <jana.rezabkova@gmail.com>
2021-11-07 16:19:42 +0100

    Abstract model superclass

---
+++ brain_connectivity/model.py
@@ -0,0 +1,12 @@
+import torch.nn as nn
+
+
+class Model(nn.Module):
+    def __init__(self):
+        super(Model, self).__init__()
+
+    def forward(self, x):
+        pass
+
+    def plot_fc_matrix(self, epoch, sublayer):
+        pass
