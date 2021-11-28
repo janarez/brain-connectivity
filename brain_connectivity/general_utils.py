ab5edd15ab4cc2d3de58bc45abb455653f6e0eff
Jana Rezabkova <jana.rezabkova@gmail.com>
2021-11-28 18:37:36 +0100

    Fix closing loggers

--- brain_connectivity/general_utils.py
+++ brain_connectivity/general_utils.py
@@ -1,27 +1,29 @@
 import logging
 
 formatter = logging.Formatter(
-    fmt="%(asctime)s %(name)-15s %(levelname)-8s %(message)s",
+    fmt="%(asctime)s %(levelname)-7s %(name)-15s %(message)s",
     datefmt="%d-%m-%Y %H:%M",
 )
 
 # Keeps track of already taken names, so that we don't attach multiple handlers to a single logger.
-logger_names = set()
-loggers = []
+loggers = {}
+
+# Root logger.
+logger = logging.getLogger("bc")
+logger.setLevel(logging.DEBUG)
 
 # Console log.
 console_handler = logging.StreamHandler()
 console_handler.setFormatter(formatter)
-
-logger = logging.getLogger("bc")
-logger.setLevel(logging.INFO)
+# Don't show debug messages.
+console_handler.setLevel(logging.INFO)
 logger.addHandler(console_handler)
 
 
 def get_logger(name: str, filename: str):
     "Returns logger that logs both to file and console at once."
     child_logger = logger.getChild(name)
-    if name in logger_names:
+    if name in loggers:
         child_logger.warning(
             f"Attempting to get logger with already taken name (name: {name}, filename: {filename}). "
             + "Not attaching new file handler."
@@ -29,19 +31,21 @@ def get_logger(name: str, filename: str):
     else:
         file_handler = logging.FileHandler(filename=filename, mode="a")
         file_handler.setFormatter(formatter)
-        # Log everything into file.
-        file_handler.setLevel(logging.DEBUG)
         child_logger.addHandler(file_handler)
-    logger_names.add(name)
-    loggers.append(child_logger)
+        loggers[name] = child_logger
 
     return child_logger
 
 
-def end_logging():
-    global logger_names, loggers
-    logger_names = set()
-    for l in loggers:
-        for h in l.handlers:
-            h.close()
-    loggers = []
+def close_logger(name: str):
+    logger = loggers.pop(name)
+    for handler in logger.handlers:
+        handler.flush()
+        handler.close()
+
+
+def close_all_loggers():
+    global loggers
+    logger_names = list(loggers.keys())
+    for name in logger_names:
+        close_logger(name)
