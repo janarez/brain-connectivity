4f64d982108a0bbd8052c8a1d01800ef94b06bfd
Jana Rezabkova <jana.rezabkova@gmail.com>
2022-04-15 21:55:49 +0200

    Add GAT model

--- brain-connectivity/brain_connectivity/models/graph.py
+++ brain-connectivity/brain_connectivity/models/graph.py
@@ -2,7 +2,7 @@ from typing import List, Union
 
 import torch
 import torch.nn as nn
-from torch_geometric.nn import GINConv, global_add_pool
+from torch_geometric.nn import GATConv, GINConv, global_add_pool
 
 from .model import Model
 
@@ -51,6 +51,45 @@ class GIN(Model):
         # Message passing.
         for conv in self.convs:
             x = conv(x, edge_index)
+        # Readout.
+        x = global_add_pool(x, batch)
+        # Classification FC.
+        x = torch.sigmoid(self.fc(x))
+
+        return x
+
+
+class GAT(Model):
+
+    hyperparameters = ["num_hidden_features", "num_sublayers"]
+
+    def __init__(
+        self,
+        size_in: int,
+        num_hidden_features: Union[List[int], int] = 90,
+        num_sublayers: int = 3,
+    ):
+        super(GAT, self).__init__()
+
+        num_in_features, num_out_features = self._mlp_dimensions(
+            size_in, num_hidden_features, num_sublayers
+        )
+
+        self.convs = torch.nn.ModuleList(
+            [
+                GATConv(in_channels=int(size_in), out_channels=size_out)
+                for size_in, size_out in zip(num_in_features, num_out_features)
+            ]
+        )
+        self.activation = nn.ReLU()
+        self.fc = nn.Linear(num_out_features[-1], 1)
+
+    def forward(self, data):
+        x, edge_index, batch = data.x, data.edge_index, data.batch
+
+        # Message passing.
+        for conv in self.convs:
+            x = self.activation(conv(x, edge_index))
         # Readout.
         x = global_add_pool(x, batch)
         # Classification FC.
