c9cbae64055d48a5577416134e162e602f625d19
Jana Rezabkova <jana.rezabkova@gmail.com>
2022-05-01 18:03:26 +0200

    Remove old scripts

--- scripts/losses.py
+++
@@ -1,8 +0,0 @@
-class DiceLoss(torch.nn.Module):
-    def __init__(self):
-        super(DiceLoss, self).__init__()
-
-    def forward(self, predict, target):
-        num = torch.sum(2*predict*target, dim=1) + 1
-        den = torch.sum(predict + target, dim=1) + 1
-        return torch.sum(1 - num / den)
